3be1cf55e3b4fe5f3d26ca0e1064cffeb8f4f2fd
Arun Chahar <110096566+arun2022cybersec@users.noreply.github.com>
2024-06-07 17:37:20 +0000

    set demo cleanup

--- sets/string_set.py
+++
@@ -1,9 +0,0 @@
-from base_set import BaseSet
-
-# class for representing the string text of various set representation and operations
-class StringSet(BaseSet):
-    def __init__(self, elements=None):
-        super().__init__(elements)
-    
-    def __str__(self):
-        return '{' + ', '.join(map(str, self.elements)) + '}'
